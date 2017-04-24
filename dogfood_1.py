46b748319f2daac0f463f69867cad0b006d4e0b3
verdejavic <victoriaverdeja@Victorias-MacBook-Pro.local>
2017-04-24 18:53:26 -0400

    get_next_page func

--- dogfood_1.py
+++ dogfood_1.py
@@ -22,26 +22,15 @@ def open_page(site):
 bsObj = open_page(site)
 f = open("food_urls.txt", "w")
 
-def get_next_page(html, bsObj):
-    #if ???
-    for i in range (1, 32):
-        new_url = "http://slimdoggy.com/dogfood/page/" + str(i) + "/?searchtype=basic&foodtype=All&foodbrand=All&foodranking=All&brandrecipesearch&ingredientsin&ingredientsout=propylene%20glycol%20AND%20ethoxyquin%20AND%20BHA%20AND%20BHT%20AND%20TBHQ%20AND%20propyl%20gallate"
-        html = urlopen(new_url)
-        bsObj = BeautifulSoup(html, "html.parser")
-        get_player_pages(html,bsObj)
-    #else ???
+#get 3rd table
+table = bsObj.findAll("table")[2]
+#get all td elements from table
+a_list = table.findAll("a")
 
-def get_player_pages(html, bsObj):
-    #get 3rd table
-    table = bsObj.findAll("table")[2]
-    #get all td elements from table
-    a_list = table.findAll("a")
-    for link in a_list:
-        if 'href' in link.attrs:
-            f.write(str(link.attrs['href']) + "\n")
-        else:
-            f.write("no href\n")
-    get_next_page(html, bsObj)
+for link in a_list:
+    if 'href' in link.attrs:
+        f.write(str(link.attrs['href']) + "\n")
+    else:
+        f.write("no href")
 
-get_player_pages(html, bsObj)
 f.close()
