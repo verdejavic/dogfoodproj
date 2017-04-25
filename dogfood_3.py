687a651909b9d9236d74fb93e11d05ad45021fc2
verdejavic <victoriaverdeja@Victorias-MacBook-Pro.local>
2017-04-24 20:11:13 -0400

    updates

--- dogfood_3.py
+++ dogfood_3.py
@@ -1,5 +1,6 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+import time
 
 hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -20,26 +21,33 @@ def open_page(site):
     return bsObj
 
 bsObj = open_page(site)
-f = open("food_urls.txt", "w")
+p = open("food_urls.txt", "w")
+food_list = []
 
-def get_next_page(site, bsObj):
+def get_next_page(html, bsObj):
     for i in range (1, 32):
         new_url = "http://slimdoggy.com/dogfood/page/" + str(i) + "/?searchtype=basic&foodtype=All&foodbrand=All&foodranking=All&brandrecipesearch&ingredientsin&ingredientsout=propylene%20glycol%20AND%20ethoxyquin%20AND%20BHA%20AND%20BHT%20AND%20TBHQ%20AND%20propyl%20gallate"
         html = urlopen(new_url)
         bsObj = BeautifulSoup(html, "html.parser")
-        get_detail_pages(bsObj)
+        get_player_pages(html,bsObj)
 
-def get_detail_pages(bsObj):
+def get_detail_pages(html, bsObj):
     #get 3rd table
     table = bsObj.findAll("table")[2]
     #get all td elements from table
     a_list = table.findAll("a")
     for link in a_list:
         if 'href' in link.attrs:
-            f.write(str(link.attrs['href']) + "\n")
-        else:
-            f.write("no href\n")
-    get_next_page(site, bsObj)
+            food_list.append(str(link.attrs['href']))
+    get_next_page(html, bsObj)
 
-get_detail_pages(bsObj)
-f.close()
+def get_food_details(food_list):
+    for i in range (1, 32):
+        new_url = "http://slimdoggy.com/dogfood/page/" + str(i) + "/?searchtype=basic&foodtype=All&foodbrand=All&foodranking=All&brandrecipesearch&ingredientsin&ingredientsout=propylene%20glycol%20AND%20ethoxyquin%20AND%20BHA%20AND%20BHT%20AND%20TBHQ%20AND%20propyl%20gallate"
+        html = urlopen(new_url)
+        bsObj = BeautifulSoup(html, "html.parser")
+        print (bsObj.find("div", {"class":"dogfooddatasheet"}))
+
+get_detail_pages(html, bsObj)
+get_food_details(food_list)
+p.close()
